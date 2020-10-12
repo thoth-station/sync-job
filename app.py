8931a81d740a52e88020d5caab52a9b326c125f7
Francesco Murdaca <fmurdaca@redhat.com>
2020-10-12 09:09:16 +0200

    Allow sync of one class of documents
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>

--- app.py
+++ app.py
@@ -19,6 +19,8 @@
 
 import logging
 
+from typing import Optional, List
+
 import click
 import thoth.storages.sync as thoth_sync_module
 from thoth.storages.sync import sync_solver_documents
@@ -37,8 +39,20 @@ _LOGGER = logging.getLogger("thoth.sync")
 @click.option("--force-sync", is_flag=True, help="Perform force sync of documents.", envvar="THOTH_SYNC_FORCE_SYNC")
 @click.option("--graceful", is_flag=True, help="Continue on any error during the sync.", envvar="THOTH_SYNC_GRACEFUL")
 @click.option("--debug", is_flag=True, help="Run in a debug mode", envvar="THOTH_SYNC_DEBUG")
-def sync(force_sync: bool, graceful: bool, debug: bool) -> None:
+@click.option(
+    "--sync-document-classes",
+    multiple=True,
+    type=str,
+    required=False,
+    help="Perform sync of specific class of documents (Expected comma separated classes)",
+    envvar="THOTH_SYNC_DOCUMENT_CLASSES",
+)
+def sync(force_sync: bool, graceful: bool, debug: bool, sync_document_classes: Optional[List[str]] = None) -> None:
     """Sync Thoth data to Thoth's knowledge base."""
+    document_classes = []
+    if sync_document_classes:
+        document_classes = sync_document_classes[0].split(",")
+
     if debug:
         _LOGGER.setLevel(logging.DEBUG)
         _LOGGER.debug("Debug mode is on.")
@@ -48,24 +62,36 @@ def sync(force_sync: bool, graceful: bool, debug: bool) -> None:
     graph = GraphDatabase()
     graph.connect()
 
-    # First we need to sync solvers
+    if not document_classes:
+        # First we need to sync solvers
 
-    stats = sync_solver_documents(force=force_sync, graceful=graceful, graph=graph)
+        stats = sync_solver_documents(force=force_sync, graceful=graceful, graph=graph)
 
-    _LOGGER.info(
-        "Syncing triggered by sync_solver_documents function completed with "
-        "%d processed, %d synced, %d skipped and %d failed documents",
-        *stats,
-    )
+        _LOGGER.info(
+            "Syncing triggered by sync_solver_documents function completed with "
+            "%d processed, %d synced, %d skipped and %d failed documents",
+            *stats,
+        )
 
     for obj_name, function in thoth_sync_module.__dict__.items():
-        if not obj_name.startswith("sync_"):
+        if not obj_name.startswith("sync_") and not document_classes:
             _LOGGER.debug("Skipping attribute %r of thoth-storages syncing module: not a syncing function", obj_name)
             continue
 
-        if obj_name == "sync_solver_documents":
+        if obj_name == "sync_solver_documents" and not document_classes:
             _LOGGER.debug("Skipping attribute %r of thoth-storages syncing module: solver already synced", obj_name)
             continue
+
+        if document_classes and obj_name in document_classes:
+            _LOGGER.info("Scheduling sync for %r using thoth-storages syncing module: %r", obj_name, function)
+            stats = function(force=force_sync, graceful=graceful, graph=graph)
+            _LOGGER.info(
+                "Syncing triggered by %r function completed with "
+                "%d processed, %d synced, %d skipped and %d failed documents",
+                obj_name,
+                *stats,
+            )
+            return
 
         stats = function(force=force_sync, graceful=graceful, graph=graph)
 
