b88045957674fa0e1a8953cd2e9c1333b125d788
Kebechet <noreply+kebechet@redhat.com>
2020-12-04 12:53:26 +0000

    Release of version 0.2.0

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ from thoth.common import init_logging
 from thoth.storages import GraphDatabase
 from thoth.storages import __version__ as __thoth_storages_version__
 
-__version__ = "0.1.2"
+__version__ = "0.2.0"
 __component_version__ = f"{__version__}+ storages{__thoth_storages_version__}"
 
 init_logging()
